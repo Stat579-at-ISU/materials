d029f77dbe73f8a27e02dca42505666744194c3f
heike <hofmann@iastate.edu>
2019-08-27 08:21:22 -0500

    intro

--- shiny/01_Hello/app.R
+++ shiny/01_Hello/app.R
@@ -14,25 +14,55 @@ ui <- fluidPage(
 		        min = 1,
 		        max = 5000,
 		        value = 2500),
-	    numericInput("mean", "Mean: ", value=0),
-	    numericInput("sd", "Standard deviation:",
+	    radioButtons("distribution", label="Select a distribution",
+	                 choices=c("Normal", "Gamma"), selected="Normal"),
+	    conditionalPanel(
+	      condition = "input.distribution== 'Normal'",
+  	    numericInput("mean", "Mean: ", value=0),
+	      numericInput("sd", "Standard deviation:",
 	                 value = 1, min=0.0001)
+	      ),
+	    conditionalPanel(
+	      condition = "input.distribution== 'Gamma'",
+	      numericInput("shape", "Shape: ", value=3),
+	      numericInput("rate", "Rate:",
+	                 value = 1, min=0.0001))
 	),
 
 	# Show a plot of the generated distribution
 	mainPanel(
-	    plotOutput("hist")
+	    plotOutput("hist1"),
+	    plotOutput("hist2")
 	)
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    output$hist <- renderPlot({
+    output$hist1 <- renderPlot({
+      dist <- NULL
+      lower <- -10
+       if(input$distribution == "Normal")
         dist <- rnorm(n = input$num, mean = input$mean, sd = input$sd)
+       else {
+        dist <- rgamma(n = input$num, shape = input$shape, rate = input$rate)
+        lower <- 0
+       }
         gg <- data.frame(dist) %>%
           ggplot(aes(x = dist)) + geom_histogram(binwidth = 0.25) +
-          xlim(c(-10,10))
+          xlim(c(lower,10))
         print(gg)
+    })
+
+    output$hist2 <- renderPlot({
+      dist <- NULL
+      if(input$distribution == "Normal")
+        dist <- rnorm(n = input$num, mean = input$mean, sd = input$sd)
+      else
+        dist <- rgamma(n = input$num, shape = input$shape, rate = input$rate)
+      gg <- data.frame(dist) %>%
+        ggplot(aes(x = dist)) + geom_histogram(binwidth = 0.25) +
+        xlim(c(-10,10))
+      print(gg)
     })
 }
 
